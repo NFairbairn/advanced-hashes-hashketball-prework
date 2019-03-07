23b75b91f6282fa7342bd67e303bb237bf62a651
Noah Fairbairn <noahfairbairn@gmail.com>
2019-03-07 18:25:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -225,6 +225,7 @@ end
 def big_shoe_rebounds
   shoe_size = 0
   player_name = " "
+  rebounds = 0
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
       if attribute == :players
