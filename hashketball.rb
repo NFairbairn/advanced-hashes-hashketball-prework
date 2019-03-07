311f998cbba9d99afbcc41f15ba4c32ff47ed578
Noah Fairbairn <noahfairbairn@gmail.com>
2019-03-07 17:08:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -210,5 +210,10 @@ def player_numbers(team_name)
 end
 
 def player_stats(player_name)
-
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+        if attribute == :players
+          data.each do |data_item, stats|
+            if data_item == player_name
+              
 end
